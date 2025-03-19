f26f0c27f483aa7da2a1b2beaf7715b0ba1d2237
abanks91 <chitownangieb@gmail.com>
2025-03-19 08:28:47 -0500

    Fixed server.js port and deployment issue

--- server.js.js
+++ server.js.js
@@ -7,7 +7,7 @@ dotenv.config(); // Load environment variables
 
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
